d2e480006df71f81fbc427d4dbf4e73eaa44dece
edatrix <lespath@gmail.com>
2015-09-14 07:30:40 -0600

    WIP - setting up mailer, custom routes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,6 +7,11 @@ class StaticController < ApplicationController
   end
 
   def contact
+    if request.
+      @contact = OpenStruct.new(params[:contact])
+      ContactFormMailer.form_submitted_email(@contact).deliver_now
+      format.html { redirect_to :back, :notice => "Your message has been submitted." }
+    end
   end
 
   def book
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   get "/", :to => "static#index"
   get "/about", :to => "static#about"
+  post 'contact', :to => "static#contact"
   get "/contact", :to => "static#contact"
   get "/the-bloodless", :to => "static#book"
+
+
 
 end
