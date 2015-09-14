6303cf02a0ddc6f2dea9375263423aa5dbb578a5
edatrix <lespath@gmail.com>
2015-09-14 16:11:42 -0600

    gets rid of contact action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,14 +6,6 @@ class StaticController < ApplicationController
   def about
   end
 
-  def contact
-    if request.
-      @contact = OpenStruct.new(params[:contact])
-      ContactFormMailer.form_submitted_email(@contact).deliver_now
-      format.html { redirect_to :back, :notice => "Your message has been submitted." }
-    end
-  end
-
   def book
   end
 end
